b5961db584a175eb70865c02d6298968780a2858
Tyler Worth <tylerworth.media@gmail.com>
2025-12-30 12:58:50 +0000

    Add catalog models and admin for products and collections

--- anarchy_and_lace/anarchy_and_lace/urls.py
+++ anarchy_and_lace/anarchy_and_lace/urls.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", include('home.urls')),
+    # later we'll add:
+    # path("", home, name="home"),
+    # path("shop/", include("catalog.urls")),
 ]
+
+# Serve media files during development only
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
