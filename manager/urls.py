ab08a4b0fec9b6690c62e274c5438c3a7fcd256a
Tyler Worth <tylerworth.media@gmail.com>
2026-01-26 19:40:24 +0000

    Added additional product and found entry missing for dashboard

--- manager/urls.py
+++ manager/urls.py
@@ -5,7 +5,7 @@ app_name = "manager"
 
 urlpatterns = [
     path("login/", views.ManagerLoginView.as_view(), name="login"),
-    path("", views.dashboard, name="dashboard"),
+    path("dashboard", views.dashboard, name="dashboard"),
     path("products/", views.product_list, name="product_list"),
     path("products/new/", views.product_create, name="product_create"),
     path("products/<int:pk>/edit/", views.product_edit, name="product_edit"),
